6c867df18e7779b98f6c4a8b87690fe1f2ced150
olivialaplaca <olivialaplaca@proton.me>
2024-09-24 15:52:37 -0400

    first commit

---
+++ grocerylist/src/main/java/com/olivia/grocerylist/GrocerylistApplication.java
@@ -0,0 +1,13 @@
+package com.olivia.grocerylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocerylistApplication.class, args);
+	}
+
+}
---
+++ grocerylist/src/test/java/com/olivia/grocerylist/GrocerylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olivia.grocerylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocerylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
