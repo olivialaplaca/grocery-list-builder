2f7e424b2b858b14b485fba93f4a8d1a042b27cb
olivialaplaca <olivialaplaca@proton.me>
2025-06-18 09:49:10 -0400

    remove thymeleaf recipe templates and modify IngredientController

--- grocerylist/src/main/java/com/olivia/grocerylist/controller/IngredientController.java
+++ grocerylist/src/main/java/com/olivia/grocerylist/controller/IngredientController.java
@@ -1,12 +1,13 @@
 package com.olivia.grocerylist.controller;
 
-import com.olivia.grocerylist.service.IngredientService;
 import com.olivia.grocerylist.db.Ingredient;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import com.olivia.grocerylist.service.IngredientService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/ingredients")
 public class IngredientController {
 
@@ -16,31 +17,38 @@ public class IngredientController {
         this.ingredientService = ingredientService;
     }
 
-    @GetMapping("/list")
-    public String listIngredients(Model theModel) {
-        var ingredientList = ingredientService.getIngredientList();
-        theModel.addAttribute("ingredients", ingredientList);
-        return "list-ingredients";
+    @GetMapping("/all")
+    public List<Ingredient> getAllIngredients() {
+        return ingredientService.getIngredientList();
     }
 
-    @GetMapping("/showAddForm")
-    public String showAddForm(Model theModel) {
-        var newIngredient = new Ingredient();
-        theModel.addAttribute("ingredient", newIngredient);
-        return "ingredient-form";
-    }
+//    @GetMapping("/list")
+//    public String listIngredients(Model theModel) {
+//        var ingredientList = ingredientService.getIngredientList();
+//        theModel.addAttribute("ingredients", ingredientList);
+//        return "list-ingredients";
+//    }
 
-    @GetMapping("/showUpdateForm")
-    public String showUpdateForm(@RequestParam("ingredientId") Long id, Model theModel) {
-        var ingredient = ingredientService.getIngredientById(id);
-        theModel.addAttribute("ingredient", ingredient);
-        return "ingredient-form";
-    }
+//    @GetMapping("/showAddForm")
+//    public String showAddForm(Model theModel) {
+//        var newIngredient = new Ingredient();
+//        theModel.addAttribute("ingredient", newIngredient);
+//        return "ingredient-form";
+//    }
 
-    @PostMapping ("/save")
-    public String saveIngredient(@ModelAttribute("ingredient") Ingredient ingredient) {
-        ingredientService.saveIngredient(ingredient);
-        return "redirect:/ingredients/list";
+//    @GetMapping("/showUpdateForm")
+//    public String showUpdateForm(@RequestParam("ingredientId") Long id, Model theModel) {
+//        var ingredient = ingredientService.getIngredientById(id);
+//        theModel.addAttribute("ingredient", ingredient);
+//        return "ingredient-form";
+//    }
+
+    @PostMapping ("/create-ingredient")
+    public ResponseEntity<Ingredient> saveIngredient(@RequestBody Ingredient ingredient) throws Exception {
+        if (ingredient.getName().isEmpty()) {
+            throw new Exception("Ingredient name must not be empty");
+        }
+        return ResponseEntity.ok().body(ingredientService.saveIngredient(ingredient));
     }
 
     @GetMapping("/delete")
