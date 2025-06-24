f3cf4b5c058000ec6043c085e200e47fc8dd60b6
olivialaplaca <olivialaplaca@proton.me>
2025-06-24 17:04:02 -0400

    clean up ingredient controller

--- grocerylist/src/main/java/com/olivia/grocerylist/controller/IngredientController.java
+++ grocerylist/src/main/java/com/olivia/grocerylist/controller/IngredientController.java
@@ -22,27 +22,6 @@ public class IngredientController {
         return ingredientService.getIngredientList();
     }
 
-//    @GetMapping("/list")
-//    public String listIngredients(Model theModel) {
-//        var ingredientList = ingredientService.getIngredientList();
-//        theModel.addAttribute("ingredients", ingredientList);
-//        return "list-ingredients";
-//    }
-
-//    @GetMapping("/showAddForm")
-//    public String showAddForm(Model theModel) {
-//        var newIngredient = new Ingredient();
-//        theModel.addAttribute("ingredient", newIngredient);
-//        return "ingredient-form";
-//    }
-
-//    @GetMapping("/showUpdateForm")
-//    public String showUpdateForm(@RequestParam("ingredientId") Long id, Model theModel) {
-//        var ingredient = ingredientService.getIngredientById(id);
-//        theModel.addAttribute("ingredient", ingredient);
-//        return "ingredient-form";
-//    }
-
     @PostMapping ("/create-ingredient")
     public ResponseEntity<Ingredient> saveIngredient(@RequestBody Ingredient ingredient) throws Exception {
         if (ingredient.getName().isEmpty()) {
